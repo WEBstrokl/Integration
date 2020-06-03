47ec8d8e48047e29021c93b41b112ecad04ce86b
Svetlana <fgoosev@gmail.com>
2020-06-03 19:51:35 +0400

    Adaptive

--- js/script.js
+++ js/script.js
@@ -18,22 +18,3 @@ $(document).ready(function(){               // карусель элементо
         dots: false
     });
   });
-
-  var acc = document.getElementsByClassName("box-main");
-  var i;
-  
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
-  
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
